26889168aec6e01b02f7a265ef28d38a067d26f2
hilmiikhsan <robbinhood22012@gmail.com>
2023-01-26 22:16:24 +0700

    add crud category

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -83,7 +83,7 @@
 
                         @can('categories.index')
                         <li class="{{ setActive('admin/category') }}"><a class="nav-link"
-                                href="#"><i class="fas fa-folder"></i>
+                                href="{{ route('admin.category.index') }}"><i class="fas fa-folder"></i>
                                 <span>Kategori</span></a></li>
                         @endcan
 
