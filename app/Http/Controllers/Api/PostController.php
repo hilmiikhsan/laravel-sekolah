1d932e6debd3e07f4e7062256a9043e1be76795f
hilmiikhsan <robbinhood22012@gmail.com>
2023-01-27 17:42:52 +0700

    add api post

--- app/Http/Controllers/Api/PostController.php
+++ app/Http/Controllers/Api/PostController.php
@@ -4,8 +4,72 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Post;
 
 class PostController extends Controller
 {
-    //
+    /**
+     * index
+     *
+     * @return void
+     */
+    public function index()
+    {
+        $posts = Post::latest()->paginate(10);
+        return response()->json([
+            "response" => [
+                "status"  => 200,
+                "message" => "List Data Posts"
+            ],
+            "data" => $posts
+        ], 200);
+    }
+
+    /**
+     * show
+     *
+     * @param  mixed $slug
+     * @return void
+     */
+    public function show($slug)
+    {
+        $post = Post::where('slug', $slug)->first();
+
+        if ($post) {
+
+            return response()->json([
+                "response" => [
+                    "status"  => 200,
+                    "message" => "Detail Data Post"
+                ],
+                "data" => $post
+            ], 200);
+        } else {
+
+            return response()->json([
+                "response" => [
+                    "status"  => 404,
+                    "message" => "Data Post Tidak Ditemukan"
+                ],
+                "data" => null
+            ], 404);
+        }
+    }
+
+    /**
+     * PostHomePage
+     *
+     * @return void
+     */
+    public function PostHomePage()
+    {
+        $posts = Post::latest()->take(6)->get();
+        return response()->json([
+            "response" => [
+                "status"  => 200,
+                "message" => "List Data Posts Homepage"
+            ],
+            "data" => $posts
+        ], 200);
+    }
 }
